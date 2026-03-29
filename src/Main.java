e43c391a260015dbca36dfdc8088d00e62a6ff1a
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-03-29 10:20:05 -0300

    numeros impares ate o num digitado

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,14 @@ public class Main {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int alcool = 0;
-        int gasolina = 0;
-        int diesel = 0;
+        System.out.println("digite um numero: ");
+        int num = input.nextInt();
 
-        System.out.println("""
-                [1]- ÁLCOOL
-                [2]- GASOLINA
-                [4]- SAIR
-                """);
-        System.out.print("DIGITE O COOMBUSTIVEL: ");
-        int tipo = input.nextInt();
-
-        while (tipo != 4) {
-
-            if (tipo == 1) {
-
-                alcool++;
-            }else if (tipo == 2) {
-                gasolina++;
+        for (int i = 1; i <= num; i++) {
+            if(i % 2 != 0){
+                System.out.println(i);
             }
-            else if (tipo == 3) {
-                diesel++;
-            }else if (tipo > 4 || tipo < 1) {
-                System.out.print("NUMERO INVÁLIDO, DIGITE NOVAMENTE");
-                tipo = input.nextInt();
-            }
-            System.out.println("""
-                [1]- ÁLCOOL
-                [2]- GASOLINA
-                [4]- SAIR
-                """);
-            System.out.print("DIGITE O COOMBUSTIVEL: ");
-            tipo = input.nextInt();
         }
-        System.out.printf("""
-                MUITO OBRIGADO
-                alcool : %d
-                gasolina : %d
-                diesel : %d
-                """,  alcool, gasolina, diesel);
     }
 
 }
