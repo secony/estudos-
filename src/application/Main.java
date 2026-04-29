ae6a1d3eb72ddc51bbdd395a1e334550ddc487ff
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-04-29 20:44:12 -0300

    avançando no estudo de arrays

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,35 @@
 
 package application;
 
+import entities.Product;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Digite a quantidade: ");
+        System.out.print("Digite a quantidade de produtos: ");
         int quantidade = input.nextInt();
 
-        int[] lista = new int[quantidade];
+        Product[] products = new Product[quantidade];
 
-        for (int i = 0; i < quantidade; i++) {
-            System.out.print("digite o numero: ");
-            lista[i] = input.nextInt();
+        for (int i = 0; i < products.length; i++) {
+            System.out.print("Digite o nome do produto: ");
+            String nome = input.next();
+
+            System.out.print("Digite o preço: ");
+            double price = input.nextDouble();
+            products[i] = new Product(nome,price);
         }
 
-        double soma = 0;
-        for (int i = 0; i < quantidade; i++) {
-            soma = soma + lista[i];
+        double soma= 0;
+
+        for (int i = 0; i < products.length; i++) {
+            soma += products[i].getPrice();
 
         }
-        double media =  soma / quantidade;
+        double media = soma/products.length;
 
-        System.out.println("Média: " + media);
-
+        System.out.println("media: "+media);
     }
 }
