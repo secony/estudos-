4faee9649c45bd1bbaef8e7912dbe1c909d1788e
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-05-11 19:49:36 -0300

    exercicio vetores (inicio)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,42 +1,27 @@
 
 package application;
-import java.util.Locale;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
 
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Quantas pessoas serao digitadas?: ");
-        int pessoas = input.nextInt();
+        System.out.print("Quantas pessoas serao digitadas? ");
+        int n  = sc.nextInt();
 
-        String nomes [] = new String [pessoas];
-        int idades [] = new int [pessoas];
-        double altura [] = new double[pessoas];
-        double media = 0;
+        String[] nome = new String[n];
+        int [] idade = new int [n];
+        double[] altura = new double[n];
 
-        for(int i = 0; i < pessoas; i++){
-            System.out.println("Dados da pessoa "+ (i+1) + " : ");
-            System.out.print("nome : ");
-            nomes[i] = input.next();
-            System.out.print("idade : ");
-            idades[i] = input.nextInt();
-            System.out.print("altura : ");
-            altura [i] = input.nextDouble();
+        for  (int i = 0; i < n; i++) {
+            System.out.println("Digite o nome da pessoa " + (i+1) + ":");
+            nome[i] = sc.next();
+            System.out.println("Digite a idade: ");
+            idade[i] = sc.nextInt();
+            System.out.println("Digite a altura: ");
+            altura[i] = sc.nextDouble();
         }
-
-        for (int i = 0; i < pessoas; i++) {
-            media = (media + altura [i]) ;
-        }
-
-        for (int i = 0; i < pessoas; i++) {
-
-
-        }
-
-
-        System.out.println("altura media: "+ media/pessoas);
     }
+
 }
