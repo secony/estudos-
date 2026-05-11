f7d751cb8e86986a7fea34de711f239fea9b6568
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-05-11 20:17:56 -0300

    achando pessoas menores que 16 anos

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,34 +2,43 @@
 package application;
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Quantas pessoas serao digitadas? ");
-        int n  = sc.nextInt();
+        int n = sc.nextInt();
 
-        String[] nome = new String[n];
-        int [] idade = new int [n];
-        double[] altura = new double[n];
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
 
-        for  (int i = 0; i < n; i++) {
-            System.out.println("Digite o nome da pessoa " + (i+1) + ":");
-            nome[i] = sc.next();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite o nomes da pessoa " + (i + 1) + ":");
+            nomes[i] = sc.next();
             System.out.println("Digite a idade: ");
-            idade[i] = sc.nextInt();
+            idades[i] = sc.nextInt();
             System.out.println("Digite a altura: ");
-            altura[i] = sc.nextDouble();
+            alturas[i] = sc.nextDouble();
         }
         double sum = 0;
 
         for (int i = 0; i < n; i++) {
-            sum += altura[i];
+            sum += alturas[i];
         }
-        double media = sum/n;
+        double media = sum / n;
 
-        System.out.println(media);
+        int pessoasMenor16 = 0;
+        for (int i = 0; i < n; i++) {
+            if (idades[i] < 16) {
+                pessoasMenor16++;
+            }
+        }
+
+        System.out.println("Pessoas menor de 16: " + pessoasMenor16);
+        System.out.println("media: "+ media);
     }
 
 }
