c329d0c5031c81ef98649ab5471617585af27c6b
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-09-03 18:11:59 -0300

    de volta

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("De volta aos estudos de Java!!!");
+
+        // Estava focando no vestibular
+    }
+}
