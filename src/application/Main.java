8e99a827c51d305b31e3b6c5014a19a0f4401b65
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-09-10 15:32:20 -0300

    .

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,41 +1,23 @@
 package application;
-import entities.Triangle;
-
+import entities.Product;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Product produto  = new Product();
         Scanner input = new Scanner(System.in);
 
-        Triangle x = new Triangle();
-        Triangle y = new Triangle();
+        System.out.println("Enter product data: ");
+        System.out.print("name: ");
+        produto.name = input.nextLine();
+        System.out.print("price: ");
+        produto.price = input.nextDouble();
+        System.out.print("quantity: ");
+        produto.quantity = input.nextInt();
 
-        System.out.println("MEDIDAS DO TRIANGULO A: ");
-        x.a =  input.nextDouble();
-        x.b = input.nextDouble();
-        x.c = input.nextDouble();
-        System.out.println("MEDIDAS DO TRIANGULO B: ");
-        y.a =  input.nextDouble();
-        y.b = input.nextDouble();
-        y.c = input.nextDouble();
-
-        double pX = (x.a + x.b + x.c)/2;
-        double pY = (y.a + y.b + y.c)/2;
-
-        double areaX = Math.sqrt (pX * (pX-x.a) * (pX-x.b) * (pX-x.c));
-        double areaY = Math.sqrt (pY * (pY-y.a) * (pY-y.b) * (pY-y.c));
-
-        System.out.println("AREA X: " + areaX);
-        System.out.println("AREA Y: " + areaY);
-
-        if  (areaX > areaY) {
-            System.out.println("TRIANGULO A É MAIOR ");
-        }
-        else if (areaX < areaY) {
-            System.out.println("TRIANGULO B É MAIOR");
-        }else {
-            System.out.println("IGUAIS");
-        }
-
+        System.out.println("Enter the number of products you want to add: ");
+        produto.quantity = input.nextInt();
+        public
     }
 }
